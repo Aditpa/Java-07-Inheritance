72e879eb936c01484751b2bf4db93ac7d0dde008
Aditpa <124455792+Aditpa@users.noreply.github.com>
2023-08-21 11:45:58 +0200

    Add files via upload

---
+++ Java 07Inheritance/src/Hangar.java
@@ -0,0 +1,13 @@
+public class Hangar {
+
+    public static void main(String[] args) {
+    Car car=new Car("Audi",60);
+    Boat boat = new Boat("Delicia",9000);
+        System.out.println(car.doStuff());
+        System.out.println(boat.doStuff());
+
+    }
+
+
+
+}
